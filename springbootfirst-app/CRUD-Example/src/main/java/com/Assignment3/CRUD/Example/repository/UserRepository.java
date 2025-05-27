d5b69892ee59b135c0e7526d96a6043ed6d73af6
DeepikaYBhat <91942838+DeepikaYBhat@users.noreply.github.com>
2025-05-27 15:52:20 +0530

    Delete springbootfirst-app/CRUD-Example directory

--- springbootfirst-app/CRUD-Example/src/main/java/com/Assignment3/CRUD/Example/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.Assignment3.CRUD.Example.repository;
-
-import com.Assignment3.CRUD.Example.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-
-}
